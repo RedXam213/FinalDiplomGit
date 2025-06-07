bed1e1b0087d0b0747cea1f2f20f0060ff3dbe0a
Afri05 <semeiname@gmail.com>
2025-06-07 05:27:34 +0300

    t

--- server/models/models.js
+++ server/models/models.js
@@ -60,6 +60,9 @@ const TypeBrand = sequelize.define("type_brand", {
 User.hasOne(Basket);
 Basket.belongsTo(User);
 
+User.hasMany(Rating);
+Rating.belongsTo(User);
+
 Basket.hasMany(BasketDevice);
 BasketDevice.belongsTo(Basket);
 
@@ -74,6 +77,9 @@ Device.belongsTo(Type);
 
 Brand.hasMany(Device);
 Device.belongsTo(Brand);
+
+Device.hasMany(Rating);
+Rating.belongsTo(Device);
 
 Type.belongsToMany(Brand, { through: TypeBrand });
 Brand.belongsToMany(Type, { through: TypeBrand });
